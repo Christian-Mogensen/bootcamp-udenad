1497ab72541742e2878c32acbf9c8e631db4310a
Christian-Mogensen <U2407192@rts-365.dk>
2021-08-19 09:37:39 +0200

    Improved solution of original function

--- eks_01c_DOMogEvents/js/index.js
+++ eks_01c_DOMogEvents/js/index.js
@@ -1,6 +1,48 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+  // stack af variabler
+  // text-count starter med 5
+  let startNumber = 5;
+  let number = startNumber;
+  let minimum = 0;
+  let maximum = 15;
 
-	// skriv din kode her...
+  let textCountElement = document.querySelector("#text-count");
+  let plusButton = document.querySelector("#btn-count-increase");
+  let minusButton = document.querySelector("#btn-count-decrease");
+
+  function updateNumber() {
+    textCountElement.textContent = number;
+  }
+  updateNumber();
+
+//   function calcNumber(event){
+// 	//   console.log(event);
+// 	  if(event.target.attributes.id.nodeValue === "btn-count-increase"){
+// 		  if(number < maximum){
+// 		  number++;
+// 		  updateNumber();
+// 		}
+// 	  } else if (event.target.attributes.id.nodeValue === "btn-count-decrease") {
+// 		if(number > minimum){  
+// 		number--;
+// 		  updateNumber();
+// 		}
+// 	  }
+//   }
+function calcNumber(direction){
+	if(direction === "inc" && number < maximum){
+		number++;
+		updateNumber();
+	}
+	if(direction === "dec" && number > minimum){
+		number--;
+		updateNumber();
+	}
+}
+
+plusButton.addEventListener("click", () => calcNumber("inc"));
+
+minusButton.addEventListener("click", () => calcNumber("dec"));
 
 }); // Afslutter: DOMContentLoaded
