9442557a965cc6f89cb68af6457645e4a7ccdf33
Christian-Mogensen <U2407192@rts-365.dk>
2021-08-19 10:24:14 +0200

    Add defer to script and function with console log

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -1,6 +1,11 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
-
-}); // Afslutter: DOMContentLoaded
+	const hilsen = "Hej med dig!"
+	function hils(){
+		console.log(hilsen);
+	}
+	hils();
+// }); 
+// Afslutter: DOMContentLoaded
