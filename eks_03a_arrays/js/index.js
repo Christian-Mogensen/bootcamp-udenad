3860903e8e4d1b91f24a6f417bac17b1d155d898
Christian-Mogensen <U2407192@rts-365.dk>
2021-08-19 14:40:23 +0200

    Add solution of assignment, use for loop to print out li innerHTML with array item

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,18 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+  // skriv din kode her...
+  const disneyFigures = ["Anders", " Fedtmule", " Mickey"];
+  const text = document.querySelector("#liste");
+  i = 0;
+  for (let i = 0; i < disneyFigures.length; i++) {
+    let disneyNameText = `${disneyFigures[i]}`;
+    if (disneyFigures[i].selected) {
+      i++;
+    }
+    text.innerHTML += "<li>" + disneyNameText + "</li>";
 
-	// skriv din kode her...
+    console.log(disneyNameText);
+  }
+
 
 }); // Afslutter: DOMContentLoaded
