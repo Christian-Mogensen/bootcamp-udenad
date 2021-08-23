9daf7bef0b8f10291206ed06859a4dfd972cf1e3
Christian-Mogensen <U2407192@rts-365.dk>
2021-08-23 08:02:10 +0200

    Add new changes

--- eks_02a_funktioner/js/index.js
+++ eks_02a_funktioner/js/index.js
@@ -2,18 +2,16 @@
 // document.addEventListener("DOMContentLoaded", () => {
 
 	// skriv din kode her...
-	const hilsen = `Hej med dig, `;
+	setInterval(
+		()=> {
+	const besked = `Hej med dig, `;
 	const navne = ["Chris", "Sheila", "Ozlem", "Vagn", "Ona"]
-	let navn = navne[~~(Math.random()*navne.length)];
-
+	const navn = navne[~~(Math.random()*navne.length)];
 	const text = document.querySelector("em");
-	function hils(navn){
-		return text.textContent = `${hilsen} ${navn}`;
-		// return `Hej med dig, ${navn}`;
-	}
-	var hilsenFraOle = hils(navn);
-	console.log(hilsenFraOle);
-	// hils("Christian");
+
+	return text.textContent = `${besked} ${navn}`;
+}
+, 1500);
 
 // }); 
 // Afslutter: DOMContentLoaded
